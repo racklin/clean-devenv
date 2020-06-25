606be62810a594de2b5ce79782c4d93455bf8e91
Rack Lin <racklin@gmail.com>
2020-06-25 17:26:08 +0800

    Added scoop.sh installer version.
    Signed-off-by: Rack Lin <racklin@gmail.com>

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,12 +3,15 @@ Import-Module posh-git
 Import-Module oh-my-posh
 Set-Theme Agnoster
 
+# alias
+Set-Alias vi vim
+
 
 # vagrant
-$ENV:VAGRANT_DEFAULT_PROVIDER = "vmware_desktop"
+#$ENV:VAGRANT_DEFAULT_PROVIDER = "vmware_desktop"
 
 #
 # docker-machine
 #
 # Run this command to configure your shell:
-& "C:\ProgramData\chocolatey\lib\docker-machine\bin\docker-machine.exe" env | Invoke-Expression
+# & "docker-machine.exe" env | Invoke-Expression
---
+++ win10_scoop_install.ps1
@@ -0,0 +1,78 @@
+#Requires -Version 5
+
+Write-Host "installing scoop..."
+if (Get-Command scoop -ErrorAction SilentlyContinue)
+{
+    Write-Host "scoop is already installed"
+} else {
+    Set-ExecutionPolicy RemoteSigned -scope CurrentUser
+    Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
+}
+
+# enabale execution policy
+Set-ExecutionPolicy RemoteSigned -scope CurrentUser
+
+
+# Basic tools
+scoop install 7zip git git-lfs sudo
+scoop install wget curl telnet ag jq
+scoop install aria2
+
+# add buckets
+Write-Host "adding scoop-extras scoop-nonportable scoop-nerd-fonts  bucket..."
+scoop bucket add extras
+scoop bucket add nonportable
+scoop bucket add nerd-fonts
+scoop bucket add java
+
+
+# install browser globally
+scoop install extras/firefox
+
+
+# Editor
+scoop install vim
+#scoop install extras/notepadplusplus
+scoop install extras/vscode
+
+
+# misc
+# scoop install extras/windows-terminal extras/vcredist2019 
+scoop install extras/powertoys
+scoop install extras/steam
+scoop install extras/vlc
+scoop install extras/foxit-reader
+scoop install extras/oh-my-posh
+scoop install screentogif
+
+
+
+# Virtualbox / Docker
+scoop install vagrant
+scoop install nonportable/virtualbox-with-extension-pack-np
+scoop install docker
+scoop install minikube kubeadm kubectl kubelet
+
+
+# Android Emulator
+scoop install nonportable/bluestacks-np
+
+# jdk and tools
+scoop install java/openjdk
+scoop install extras/sourcetrail
+
+# uml tools
+scoop install plantuml
+scoop install graphviz
+
+
+# jetbrains
+scoop install extras/jetbrains-toolbox
+
+
+# nerdfonts
+sudo scoop install nerd-fonts/JetBrains-Mono
+
+# python and tools
+scoop install python
+pip install httpie
